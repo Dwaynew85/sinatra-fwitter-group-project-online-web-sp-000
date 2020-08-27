700029b6aeca188bc30fbf4c19788361850d853f
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-08-27 07:51:59 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,38 +1,38 @@
 class UsersController < ApplicationController
-
   get '/users/:slug' do
     @user = User.find_by_slug(params[:slug])
     erb :'users/show'
   end
 
   get '/signup' do
-    if logged_in?
-      redirect to '/tweets'
+    if !logged_in?
+      erb :'users/create_user', locals: {message: "Please sign up before you sign in"}
     else
-      erb :"/users/create_user"
+      redirect to '/tweets'
     end
   end
 
   post '/signup' do
-    if params[:username] =="" || params[:email] == "" || params[:password] == ""
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
       redirect to '/signup'
     else
-      @user = User.create(params)
+      @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+      @user.save
       session[:user_id] = @user.id
       redirect to '/tweets'
     end
   end
 
   get '/login' do
-    if logged_in?
-      redirect to '/tweets'
+    if !logged_in?
+      erb :'users/login'
     else
-      erb :'/users/login'
+      redirect to '/tweets'
     end
   end
 
   post '/login' do
-    user = User.find_by(email: params[:email])
+    user = User.find_by(:username => params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect to "/tweets"
@@ -43,11 +43,10 @@ class UsersController < ApplicationController
 
   get '/logout' do
     if logged_in?
-      session.clear
-      redirect '/login'
+      session.destroy
+      redirect to '/login'
     else
       redirect to '/'
     end
   end
-
 end
